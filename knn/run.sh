d22ec4203dba8457c30de3968ee7cd688e780d4e
Jason Fan <sevengram1991@gmail.com>
2015-09-03 10:05:53 -0600

    update run.sh

--- knn/run.sh
+++ knn/run.sh
@@ -2,10 +2,12 @@
 
 for ((i=3; i<=200; i=i+10))
 do
-    python knn.py --k=${i} --limit 1000 >> 1.txt
+    echo k=${i} limit=1000
+    python knn.py --k=${i} --limit 1000
 done
 
 for ((i=50; i<=2000; i=i+50))
 do
-    python knn.py --k=3 --limit ${i} >> 2.txt
+    echo k=3 limit=${i}
+    python knn.py --k=3 --limit ${i}
 done
