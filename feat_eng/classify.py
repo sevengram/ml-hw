33952f7b9b48ee0771feda2792abdd7f36cf9e96
Jason Fan <sevengram1991@gmail.com>
2015-09-14 17:53:28 -0600

    Update feat_eng
    1. add arguments
    2. use 1-3-grams

--- feat_eng/classify.py
+++ feat_eng/classify.py
@@ -1,6 +1,11 @@
+__author__ = "Jianxiang Fan"
+__email__ = "jianxiang.fan@colorado.edu"
+
+import argparse
 from csv import DictReader, DictWriter
 
 import numpy as np
+from sklearn.cross_validation import cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import SGDClassifier
 
@@ -8,9 +13,25 @@ kTARGET_FIELD = 'spoiler'
 kTEXT_FIELD = 'sentence'
 
 
+class DiscussVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        """
+        Return a callable that handles preprocessing and tokenization
+        """
+        preprocess = self.build_preprocessor()
+
+        if self.analyzer == 'word':
+            stop_words = self.get_stop_words()
+            tokenize = self.build_tokenizer()
+            return lambda doc: self._word_ngrams(
+                tokenize(preprocess(self.decode(doc))), stop_words)
+        else:
+            raise ValueError('%s is not a valid tokenization scheme/analyzer' % self.analyzer)
+
+
 class Featurizer:
     def __init__(self):
-        self.vectorizer = CountVectorizer()
+        self.vectorizer = DiscussVectorizer(ngram_range=(1, 3))
 
     def train_feature(self, examples):
         return self.vectorizer.fit_transform(examples)
@@ -18,49 +39,61 @@ class Featurizer:
     def test_feature(self, examples):
         return self.vectorizer.transform(examples)
 
-    def show_top10(self, classifier, categories):
+    def show_top(self, classifier, categories, n=20):
         feature_names = np.asarray(self.vectorizer.get_feature_names())
         if len(categories) == 2:
-            top10 = np.argsort(classifier.coef_[0])[-10:]
-            bottom10 = np.argsort(classifier.coef_[0])[:10]
-            print("Pos: %s" % " ".join(feature_names[top10]))
-            print("Neg: %s" % " ".join(feature_names[bottom10]))
+            top = np.argsort(classifier.coef_[0])[-n:]
+            bottom = np.argsort(classifier.coef_[0])[:n]
+            print('Pos: %s' % ' '.join(feature_names[top]))
+            print('Neg: %s' % ' '.join(feature_names[bottom]))
         else:
             for i, category in enumerate(categories):
-                top10 = np.argsort(classifier.coef_[i])[-10:]
-                print("%s: %s" % (category, " ".join(feature_names[top10])))
+                top = np.argsort(classifier.coef_[i])[-n:]
+                print('%s: %s' % (category, ' '.join(feature_names[top])))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--val', help='Run cross validation after training', action='store_true')
+    parser.add_argument('--cv', help='Number of folds in cross validation',
+                        type=int, default=10, required=False)
+    parser.add_argument('--top', help='Number of top features to show',
+                        type=int, default=20, required=False)
+    parser.add_argument('--output', help='Output file',
+                        type=str, default='predictions.csv', required=False)
+    parser.add_argument('--train', help='Training set file',
+                        type=str, default='../data/spoilers/train.csv', required=False)
+    parser.add_argument('--test', help='Test set file',
+                        type=str, default='../data/spoilers/test.csv', required=False)
+    args = parser.parse_args()
+
     # Cast to list to keep it all in memory
-    train = list(DictReader(open("../data/spoilers/train.csv", 'r')))
-    test = list(DictReader(open("../data/spoilers/test.csv", 'r')))
-
-    feat = Featurizer()
-
+    train = list(DictReader(open(args.train, 'r')))
+    test = list(DictReader(open(args.test, 'r')))
     labels = []
     for line in train:
         if not line[kTARGET_FIELD] in labels:
             labels.append(line[kTARGET_FIELD])
 
-    print("Label set: %s" % str(labels))
-    x_train = feat.train_feature(x[kTEXT_FIELD] for x in train)
-    x_test = feat.test_feature(x[kTEXT_FIELD] for x in test)
-
+    feat = Featurizer()
+    print('Label set: %s' % str(labels))
+    x_train = feat.train_feature([x[kTEXT_FIELD] for x in train])
+    x_test = feat.test_feature([x[kTEXT_FIELD] for x in test])
     y_train = np.array(list(labels.index(x[kTARGET_FIELD]) for x in train))
 
-    print(len(train), len(y_train))
-    print(set(y_train))
-
-    # Train classifier
+    # Train classifier & make cross validation
     lr = SGDClassifier(loss='log', penalty='l2', shuffle=True)
-    lr.fit(x_train, y_train)
-
-    feat.show_top10(lr, labels)
-
-    predictions = lr.predict(x_test)
-    o = DictWriter(open("predictions.csv", 'w'), ["id", "cat"])
-    o.writeheader()
-    for ii, pp in zip([x['id'] for x in test], predictions):
-        d = {'id': ii, 'cat': labels[pp]}
-        o.writerow(d)
+    if args.val:
+        scores = cross_val_score(lr, x_train, y_train, scoring='f1', cv=args.cv)
+        print(scores)
+        print(scores.mean())
+        print(scores.std())
+    else:
+        lr.fit(x_train, y_train)
+        feat.show_top(lr, labels, args.top)
+        predictions = lr.predict(x_test)
+        o = DictWriter(open(args.output, 'w'), ['id', 'spoiler'])
+        o.writeheader()
+        for ii, pp in zip([x['id'] for x in test], predictions):
+            d = {'id': ii, 'spoiler': labels[pp]}
+            o.writerow(d)
