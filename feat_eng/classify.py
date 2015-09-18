37c344759748800eab39b352160234181c0ab28a
Jason Fan <sevengram1991@gmail.com>
2015-09-17 22:26:40 -0600

    Update feat_eng
    add meta data flags

--- feat_eng/classify.py
+++ feat_eng/classify.py
@@ -2,8 +2,8 @@ __author__ = "Jianxiang Fan"
 __email__ = "jianxiang.fan@colorado.edu"
 
 import argparse
-from csv import DictReader, DictWriter
 import itertools
+from csv import DictReader, DictWriter
 
 import nltk
 import numpy as np
@@ -11,11 +11,11 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, VectorizerMixin
 from sklearn.linear_model import SGDClassifier
 
+kPAGE_FIELD = 'page'
 kTARGET_FIELD = 'spoiler'
 kTEXT_FIELD = 'sentence'
 
 first_person_words = ['i', 'we', 'you', 'us', 'my', 'mine', 'your', 'yours']
-
 third_person_words = ['he', 'she', 'they', 'their', 'his', 'him', 'her', 'theirs', 'hers']
 
 
@@ -24,24 +24,31 @@ class SentenceMixin(VectorizerMixin):
         """
         Return a callable that handles preprocessing and tokenization
         """
-        if callable(self.analyzer):
-            return self.analyzer
-
         preprocess = self.build_preprocessor()
         tokenize = self.build_tokenizer()
-        pre_token = lambda doc: tokenize(preprocess(self.decode(doc)))
 
-        feature_map = {
-            'length': lambda doc: ['L:%d' % (len(pre_token(doc)) / 5)],
-            '1st': lambda doc: ('T:1st' for i in pre_token(doc) if i in first_person_words),
-            '3rd': lambda doc: ('T:3rd' for i in pre_token(doc) if i in third_person_words),
-            'word': lambda doc: self._word_ngrams(pre_token(doc), self.get_stop_words()),
-            'tag': lambda doc: self._word_ngrams([t[1] for t in nltk.pos_tag(pre_token(doc))])
+        filter_meta = lambda doc: ' '.join([w for w in doc.split() if not w.startswith('~')])
+        parse_words = lambda doc: tokenize(preprocess(filter_meta(self.decode(doc))))
+        meta_func = lambda prefix: lambda doc: (t for t in self.decode(doc).split() if t.startswith(prefix))
+
+        feat_func_map = {
+            'length': lambda doc: ['~L:%d' % (len(parse_words(doc)) / 5)],
+            '1st': lambda doc: ('~T:1st' for i in parse_words(doc) if i in first_person_words),
+            '3rd': lambda doc: ('~T:3rd' for i in parse_words(doc) if i in third_person_words),
+            'word': lambda doc: self._word_ngrams(parse_words(doc), self.get_stop_words()),
+            'tag': lambda doc: self._word_ngrams([t[1] for t in nltk.pos_tag(parse_words(doc))]),
+            'genre': meta_func('~G'),
+            'rating': meta_func('~Ra'),
+            'votes': meta_func('~V'),
+            'lang': meta_func('~La'),
+            'country': meta_func('~Co'),
+            'year': meta_func('~Y'),
+            'runtime': meta_func('~Rt')
         }
-
         func_list = None
         if type(self.analyzer) is str:
-            func_list = [feature_map[flag.strip()] for flag in self.analyzer.split(':') if flag.strip() in feature_map]
+            func_list = [feat_func_map[flag.strip()] for flag in self.analyzer.split(':') if
+                         flag.strip() in feat_func_map]
         if not func_list:
             raise ValueError('%s is not a valid tokenization scheme/analyzer' % self.analyzer)
         else:
@@ -56,21 +63,46 @@ class TfidfSentenceVectorizer(TfidfVectorizer, SentenceMixin):
     pass
 
 
+def create_documents(examples, metainfo=None):
+    for e in examples:
+        doc = e[kTEXT_FIELD]
+        page = e[kPAGE_FIELD]
+        if metainfo and page in metainfo:
+            for v in metainfo[page]['genre']:
+                if v.upper() != 'N/A':
+                    doc += " ~G:" + v
+            for v in metainfo[page]['lang']:
+                if v.upper() != 'N/A':
+                    doc += " ~La:" + v
+            for v in metainfo[page]['country']:
+                if v.upper() != 'N/A':
+                    doc += " ~Co:" + v
+            if metainfo[page]['rating']:
+                doc += " ~Ra:%s" % metainfo[page]['rating']
+            if metainfo[page]['votes']:
+                doc += " ~V:%s" % metainfo[page]['votes']
+            if metainfo[page]['year']:
+                doc += " ~Y:%s" % metainfo[page]['year']
+            if metainfo[page]['runtime']:
+                doc += " ~Rt:%s" % metainfo[page]['runtime']
+        yield doc
+
+
 class Featurizer:
     def __init__(self, min_n, max_n, analyzer, use_tfidf=False):
         self.vectorizer = SentenceVectorizer(analyzer=analyzer, ngram_range=(min_n, max_n)) if not use_tfidf \
             else TfidfSentenceVectorizer(analyzer=analyzer, ngram_range=(min_n, max_n))
 
-    def train_feature(self, examples):
-        return self.vectorizer.fit_transform(examples)
+    def train_feature(self, examples, metainfo=None):
+        return self.vectorizer.fit_transform(create_documents(examples, metainfo))
 
-    def test_feature(self, examples):
-        return self.vectorizer.transform(examples)
+    def test_feature(self, examples, metainfo=None):
+        return self.vectorizer.transform(create_documents(examples, metainfo))
 
     def show_top(self, classifier, categories, n):
         feature_names = np.asarray(self.vectorizer.get_feature_names())
         if len(categories) == 2:
-            top = np.argsort(classifier.coef_[0])[-n:]
+            top = np.argsort(classifier.coef_[0])[-n:][::-1]
             bottom = np.argsort(classifier.coef_[0])[:n]
             print('Pos: %s' % ','.join(feature_names[top]))
             print('Neg: %s' % ','.join(feature_names[bottom]))
@@ -82,25 +114,55 @@ class Featurizer:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--val', help='Run cross validation after training', action='store_true')
+    parser.add_argument('--val', help='Run cross validation on training set', action='store_true')
     parser.add_argument('--cv', help='Number of folds in cross validation',
-                        type=int, default=5, required=False)
+                        type=int, default=10, required=False)
     parser.add_argument('--top', help='Number of top features to show',
-                        type=int, default=20, required=False)
+                        type=int, default=50, required=False)
     parser.add_argument('--output', help='Output file',
                         type=str, default='predictions.csv', required=False)
     parser.add_argument('--train', help='Training set file',
                         type=str, default='../data/spoilers/train.csv', required=False)
     parser.add_argument('--test', help='Test set file',
                         type=str, default='../data/spoilers/test.csv', required=False)
-    parser.add_argument('--feat', help='Features concat by :',
-                        type=str, default='word', required=False)
+    parser.add_argument('--meta', help='Meta info file',
+                        type=str, default='./info.csv', required=False)
     parser.add_argument('--ngmin', help='The lower boundary of the range of n-values for n-grams',
                         type=int, default=1, required=False)
     parser.add_argument('--ngmax', help='The upper boundary of the range of n-values for n-grams',
                         type=int, default=1, required=False)
+    parser.add_argument('--feat', help='Features concat by \':\'',
+                        type=str, default='word', required=False)
     parser.add_argument("--tfidf", help="Use tf-idf", action='store_true')
     args = parser.parse_args()
+
+    # Load meta data
+    meta = {}
+    all_votes = []
+    split_group = lambda di, key: di.get(key, '').replace(' ', '').split(',')
+    if args.meta:
+        meta_raw = list(DictReader(open(args.meta, 'r')))
+        for m in meta_raw:
+            meta[m['page']] = {}
+            rating = m.get('imdbRating', 'N/A')
+            release = m.get('Released', 'N/A')
+            runtime = m.get('Runtime', 'N/A')
+            votes = str(m.get('imdbVotes', 'N/A')).replace(',', '')
+            if votes != 'N/A':
+                all_votes.append(int(votes))
+            meta[m['page']]['genre'] = split_group(m, 'Genre')
+            meta[m['page']]['year'] = int(release.split()[-1]) / 20 * 20 if release != 'N/A' else None
+            meta[m['page']]['runtime'] = int(runtime.split()[0]) / 30 + 1 if runtime != 'N/A' else None
+            meta[m['page']]['lang'] = split_group(m, 'Language')
+            meta[m['page']]['country'] = split_group(m, 'Country')
+            meta[m['page']]['rating'] = int(float(rating)) + 1 if rating != 'N/A' else None
+            meta[m['page']]['votes'] = int(votes) if votes != 'N/A' else None
+        all_votes.sort()
+        score_votes = lambda s: all_votes.index(s) / (len(all_votes) / 10)
+        for p, m in meta.iteritems():
+            real_votes = m['votes']
+            m['votes'] = score_votes(real_votes) + 1 if real_votes else None
+    print('Meta data loaded!')
 
     # Cast to list to keep it all in memory
     train = list(DictReader(open(args.train, 'r')))
@@ -109,10 +171,10 @@ if __name__ == '__main__':
     for line in train:
         if not line[kTARGET_FIELD] in labels:
             labels.append(line[kTARGET_FIELD])
+    print('Text data loaded!')
 
     feat = Featurizer(args.ngmin, args.ngmax, args.feat, args.tfidf)
-    print('Label set: %s' % str(labels))
-    x_train = feat.train_feature(x[kTEXT_FIELD] for x in train)
+    x_train = feat.train_feature(train, meta)
     y_train = np.array(list(labels.index(x[kTARGET_FIELD]) for x in train))
 
     # Train classifier & make cross validation
@@ -122,13 +184,14 @@ if __name__ == '__main__':
         print(scores)
         print(scores.mean())
         print(scores.std())
-    else:
-        x_test = feat.test_feature(x[kTEXT_FIELD] for x in test)
-        lr.fit(x_train, y_train)
-        feat.show_top(lr, labels, args.top)
-        predictions = lr.predict(x_test)
-        o = DictWriter(open(args.output, 'w'), ['id', 'spoiler'])
-        o.writeheader()
-        for ii, pp in zip([x['id'] for x in test], predictions):
-            d = {'id': ii, 'spoiler': labels[pp]}
-            o.writerow(d)
+
+    # Run test data
+    x_test = feat.test_feature(test, meta)
+    lr.fit(x_train, y_train)
+    feat.show_top(lr, labels, args.top)
+    predictions = lr.predict(x_test)
+    o = DictWriter(open(args.output, 'w'), ['id', 'spoiler'])
+    o.writeheader()
+    for ii, pp in zip([x['id'] for x in test], predictions):
+        d = {'id': ii, 'spoiler': labels[pp]}
+        o.writerow(d)
--- feat_eng/imdb.py
+++ feat_eng/imdb.py
@@ -1,17 +1,33 @@
-from csv import DictReader, DictWriter
-
 __author__ = "Jianxiang Fan"
 __email__ = "jianxiang.fan@colorado.edu"
 
-import json
+import sys
 import re
+import json
 import urllib2
 import argparse
+from csv import DictReader, DictWriter
+
 from bs4 import BeautifulSoup
 
 
 def get_query_string(name):
+    name = name.replace('Remake', '').replace('Reimagined', '')
+    name = re.sub('[0-9]+$', '', name)
     return '+'.join(re.findall('[A-Z][^A-Z]*', name))
+
+
+def almost_same(n1, n2):
+    if not n1 or not n2:
+        return False
+    n1 = n1.lower()
+    n2 = n2.lower()
+    n1_first_half = n1[:(len(n1) + 1) / 2]
+    n2_first_half = n2[:(len(n2) + 1) / 2]
+    n1_second_half = n1[len(n1) / 2:]
+    n2_second_half = n2[len(n2) / 2:]
+    return n1 == n2 or n1.startswith(n2_first_half) or n2.startswith(n1_first_half) or n1.endswith(
+        n2_second_half) or n2.endswith(n1_second_half)
 
 
 def fetch_imdb_info(movie_name):
@@ -19,15 +35,18 @@ def fetch_imdb_info(movie_name):
     if resp['Response'] == 'True':
         return resp
     imdb_rearch = BeautifulSoup(
-        urllib2.urlopen('http://www.imdb.com/find?q=%s' % movie_name).read())
-    links = [td.a for td in imdb_rearch.findAll('td', {'class': 'result_text'})]
-    for l in links:
-        if re.sub('[^A-Za-z0-9]+', '', l.text.replace('&', 'and')).lower() == movie_name.lower():
-            imdbid = l['href'].split('/')[2]
+        urllib2.urlopen('http://www.imdb.com/find?q=%s' % get_query_string(movie_name)).read(), "html5lib")
+    links = [(td.a, td.i) for td in imdb_rearch.findAll('td', {'class': 'result_text'})]
+    for la, li in links:
+        imdb_name = re.sub('[^A-Za-z0-9]+', '', la.text.replace('&', 'and'))
+        aka_name = re.sub('[^A-Za-z0-9]+', '', li.text) if li else ''
+        if almost_same(imdb_name, movie_name) or almost_same(aka_name, movie_name):
+            imdbid = la['href'].split('/')[2]
             resp = json.loads(urllib2.urlopen('http://www.omdbapi.com/?i=%s' % imdbid).read())
             if resp['Response'] == 'True':
                 return resp
     print('%s not found' % movie_name)
+    sys.stdout.flush()
     return None
 
 
