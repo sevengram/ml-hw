7f15ecb8b1d10fafe37a5d8a496227ad678b42a1
Jason Fan <sevengram1991@gmail.com>
2015-12-02 13:32:37 -0700

    finish TODO in vb

--- vb/test.py
+++ vb/test.py
@@ -3,8 +3,6 @@ import unittest
 from numpy import array
 
 from lda import VariationalBayes
-from scipy.special import psi as digam
-from math import exp
 
 
 class TestVB(unittest.TestCase):
@@ -15,11 +13,9 @@ class TestVB(unittest.TestCase):
 
     def test_single_phi(self):
         vb = VariationalBayes()
-
         gamma = array([2.0, 2.0, 2.0])
         beta = self.init_beta
         phi = vb.new_phi(gamma, beta, 0, 1)
-
         prop = 0.27711205238850234
         normalizer = sum(x * prop for x in beta[:, 0])
         self.assertAlmostEqual(phi[0], beta[0][0] * prop / normalizer)
@@ -28,23 +24,19 @@ class TestVB(unittest.TestCase):
 
     def test_multiple_phi(self):
         vb = VariationalBayes()
-
         gamma = array([2.0, 2.0, 2.0])
         beta = self.init_beta
         phi = vb.new_phi(gamma, beta, 0, 2)
-
         prop = 0.27711205238850234
         normalizer = sum(x * prop for x in beta[:, 0]) / 2.0
         self.assertAlmostEqual(phi[0], beta[0][0] * prop / normalizer)
         self.assertAlmostEqual(phi[1], beta[1][0] * prop / normalizer)
         self.assertAlmostEqual(phi[2], beta[2][0] * prop / normalizer)
 
-
     def test_m(self):
         vb = VariationalBayes()
         vb.init([], "stuck", 3)
         # vb.m_step(self.init_beta)
-
         # self.assertAlmostEqual(self.init_beta[2][3], vb._beta[2][3])
 
         topic_count = array([[5., 4., 3., 2., 1.],
@@ -56,6 +48,7 @@ class TestVB(unittest.TestCase):
         self.assertAlmostEqual(vb._beta[0][0], .33333333)
         self.assertAlmostEqual(vb._beta[1][4], .11111111)
         self.assertAlmostEqual(vb._beta[0][3], .13333333)
+
 
 if __name__ == '__main__':
     unittest.main()
